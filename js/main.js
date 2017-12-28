a3d013e612dd64a8bd6f5eaaca0f9e13cd7e0e8c
whg <wanghg@skylinematrix.com>
2017-12-29 05:35:09 +0800

    hello world但是手机浏览看不到渐变的"微信小游戏"字体

--- js/main.js
+++ js/main.js
@@ -9,16 +9,29 @@ export default class Main {
 
     paint() {
         ctx.font = "20px Georgia"
+        ctx.fillStyle = '#87CEFF'
         ctx.fillText('hello world', 10, 50)
 
         ctx.font = "30px Verdana"
         // 创建渐变
         var gradient = ctx.createLinearGradient(0, 0, window.innerWidth, 0)
-        gradient.addColorStop("0","magenta")
-        gradient.addColorStop("0.5","blue")
-        gradient.addColorStop("1.0","red")
+        gradient.addColorStop("0", "magenta")
+        gradient.addColorStop("0.5", "blue")
+        gradient.addColorStop("1.0", "red")
         // 用渐变填色
         ctx.fillStyle = gradient
         ctx.fillText("微信小游戏", 10, 90)
+
+        window.requestAnimationFrame(
+          this.loop.bind(this),
+          canvas
+        )
+    }
+
+    loop() {
+      window.requestAnimationFrame(
+        this.loop.bind(this),
+        canvas
+      )
     }
 }
